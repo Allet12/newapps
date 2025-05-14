6c58cbb0daea7721789c8fabd6731f5186e99920
Allet12 <144069058+Allet12@users.noreply.github.com>
2025-05-14 19:06:05 +0200

    Update User.js

--- back-end/model/User.js
+++ back-end/model/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  pin: { type: String, required: true},
-  password: { type: String, required: true},
+  username: { type: String, required: true,unique:true },
+  pin: { type: String, required: true,unique:true},
+  password: { type: String, required: true,unique:true},
   
 
 });
