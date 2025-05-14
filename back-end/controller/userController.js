fa0d9e6d5dc0296c5020cc7126b70f4559463a3b
mashomanye <221145125@mycput.ac.za>
2025-05-14 16:54:19 +0200

    removed user finder

--- back-end/controller/userController.js
+++ back-end/controller/userController.js
@@ -3,10 +3,10 @@ async function register(req, res) {
   try {
     const { username, pin, password } = req.body;
 
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
+   /* const existingUser = await User.findOne({ username });
+   if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
-    }
+    }*/
 
   
     const newUser = new User({ username, pin, password });
